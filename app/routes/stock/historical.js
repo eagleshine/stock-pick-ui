af4c033e671a158b1386ca518d23d3efcfb5f4b8
Huibing Yin <huibingy@yahoo-inc.com>
2017-09-13 00:51:43 -0500

    Adding historical route. The ember-techan is outdated

--- app/routes/stock/historical.js
+++ app/routes/stock/historical.js
@@ -6,10 +6,46 @@ export default Ember.Route.extend({
   },
 
   setupController(controller, model) {
-    let historical = model.get('result');
-    console.log(historical);
+    let historical = model.get('result')[0];
+    //console.log(historical);
+    let meta = historical.meta;
+    let timestamp = historical.timestamp;
+    let events = historical.events;
+    let quote = historical.indicators.quote[0];
 
-    controller.set('historical', historical);
+    let data = [];
+    for (let i = 0; i < timestamp.length; i++) {
+      data.push({
+        date: new Date(timestamp[i]*1000).toLocaleDateString('en-GB', {
+          day : 'numeric',
+          month : 'short',
+          year : '2-digit'
+        }).split(' ').join('-'),
+        open: +quote.open[i],
+        high: +quote.high[i],
+        low: +quote.low[i],
+        close: +quote.close[i],
+        volume: +quote.volume[i]
+      });
+    }
+
+    const width = window.screen.availWidth * 0.79;
+
+    controller.set('meta', meta);
+    controller.set('events', events);
+    controller.set('data', data);
+    controller.set('width', width);
     controller.set('hist', JSON.stringify(historical, null, 4));
+
+    console.log(data);
+  },
+
+  afterModel(model, transition) {
+    const _this = this;
+    transition.then(function() {
+      model.currentUrl = _this.get('router.url');
+      console.log(_this.get('router.url'));
+      console.log(model.currentUrl);
+    });
   }
 });
